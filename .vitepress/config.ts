3128e6b96d4322abd597e7c6ad895d22c27185f6
待定红尘 <1445493904@qq.com>
2024-06-01 12:25:20 +0800

    feat:增加发布到vercel配置

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
     ]
   },
-  base: process.env.NODE_ENV !== 'production' ? "/th-ui/" : '/',
+  base: process.env.NODE_ENV !== 'production' && !process.env.BUILD_VERCEL ? "/th-ui/" : '/',
   rewrites: {
     'docs/(.*)': '(.*)',
     'packages/th-ui/src/:comp/(.*)': 'components/:comp/(.*)',
